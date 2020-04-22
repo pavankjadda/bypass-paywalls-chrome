5f8502a4a955d27979b274ac0d1e9fececfa4266
Pavan Jadda <jaddap2@nih.gov>
2020-04-22 11:12:09 -0400

    Formatted code with Google JS style

--- src/js/common.js
+++ src/js/common.js
@@ -5,4 +5,5 @@ const extension_api =
         (typeof chrome === 'object' &&
             typeof chrome.runtime === 'object' &&
             typeof chrome.runtime.getManifest === 'function') ? chrome :
-            console.log('Cannot find extension_api under namespace "browser" or "chrome"');
+            console.log(
+                'Cannot find extension_api under namespace "browser" or "chrome"');
